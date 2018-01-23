c772f7a56afc2ac2a9644c90ed09aa158f5a4ac4
郭堯彰 <YaoChang@guoyaozhangde-MacBook-Pro.local>
2018-01-23 17:54:09 +0800

    Update iOS Framework 2.0.0

--- CFiBannerSample/ViewController.m
+++ CFiBannerSample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AdSupport/ASIdentifierManager.h>
 
 @interface ViewController ()
 
@@ -17,14 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
+
     CGRect r = self.view.frame;
     
     banner = [[MFBannerView alloc] initWithAdSize:MFAdSize320X50 origin:CGPointMake((r.size.width-320)/2, r.size.height-50)];
-    
     [banner setBannerAlignment:Alignment_Center];
-    [banner setAdAutoRefreshTime:30];
-    [banner setBannerBackgroudColor:[UIColor redColor]];
+    [banner setAdAutoRefreshTime:0];
     banner.bannerId = @"3783" ;
     banner.delegate = self;
     [self.view addSubview:banner];
@@ -37,6 +36,15 @@
     [banner show];
 }
 
+- (void)requestAdFail
+{
+    NSLog(@"廣告請求失敗");
+}
+
+- (void)onClickAd
+{
+    NSLog(@"Click Ad");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
